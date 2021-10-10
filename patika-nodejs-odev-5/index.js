ab8144bcb029d08912ff8eabb50dbe410557abc4
ataberkdw <ataberkturkoglug3@gmail.com>
2021-10-10 14:13:10 +0300

    patika=nodejs-odev-5

---
+++ patika-nodejs-odev-5/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+    if(url==='/'){
+        res.writeHead(200,{'Content-type':'text/html'});
+        res.write('<h2>Index sayfasina hos geldiniz<h2>')
+    }else if(url==='/hakkimda'){
+        res.writeHead(200,{'Content-type':'text/html'});
+        res.write('<h2>Hakkimda sayfasina hos geldiniz<h2>')
+    }else if(url==='/iletisim'){
+        res.writeHead(200,{'Content-type':'text/html'});
+        res.write('<h2>Iletisim sayfasina hos geldiniz<h2>')
+    }else{
+        res.writeHead(400,{'Content-type':'text/html'});
+        res.write('<h2>404 not found<h2>')
+    }
+    res.end()
+})
+
+const port = 5000;
+server.listen(5000);
+
